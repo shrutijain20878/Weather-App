643e957693cfbdda3faa9ecf92e973e443dead2f
Shruti Jain <shrutijain20878@gmail.com>
2023-04-07 12:44:21 +0530

    first changes

---
+++ src/weatherService.js
@@ -0,0 +1,41 @@
+const API_KEY = 
+'444e73b802d023e79a37b0711a643ba1';
+
+const makeIconURL = (iconId) => `https://openweathermap.org
+/img/wn/${iconId}@2x.png`
+
+const getFormattedWeatherData = async (city, units = 'metric') => {
+
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=
+    ${city}&appid=${API_KEY}&units=${units}`;
+
+    const data=await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+    const {
+        weather, 
+        main: {temp, feels_like, temp_min, temp_max,
+            pressure, humidity},
+    wind: {speed},
+    sys: {country},
+    name,
+} = data;
+
+const {description, icon} = weather[0]
+return{
+   description,
+    iconURL: makeIconURL(icon), 
+    temp,
+     feels_like,
+      temp_min,
+   temp_max, 
+   pressure, 
+   humidity, 
+   speed, 
+   country, 
+   name,
+}
+}
+
+export {getFormattedWeatherData};
